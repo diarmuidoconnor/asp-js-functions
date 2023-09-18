cbcd45dfd1f87abe095c7f76b786847b9972e3c3
Diarmuid O Connor <doconnor@wit.ie>
2023-09-18 09:48:59 +0100

    Uncomment reducer code

--- 05_reducing.js
+++ 05_reducing.js
@@ -19,37 +19,37 @@ fetch("https://randomuser.me/api/?results=10")
     console.log(`Average age =  ${Math.round(ageAccumulator/ profiles.length)}`);
 
     // // Objective: Find youngest profile.
-    // const youngest = profiles.reduce( (min, profile) => {
-    //     const newMin = profile.dob.age < min ?  profile.dob.age : min
-    //     // console.log(newMin )
-    //     return newMin
-    // },100 )
-    // console.log(`Youngest = ${youngest}`)
+    const youngest = profiles.reduce( (min, profile) => {
+        const newMin = profile.dob.age < min ?  profile.dob.age : min
+        // console.log(newMin )
+        return newMin
+    },100 )
+    console.log(`Youngest = ${youngest}`)
 
-    // // Objective: Compute no. of profiles in each age groups.
-    // const ageGroups = {
-    //   '25-': 0,
-    //   '26-50': 0,
-    //   '51-75': 0,
-    //   '76+': 0
-    // };
-    // const ageCategories = Object.keys(ageGroups);
-    // // console.log(ageCategories);
+    // Objective: Compute no. of profiles in each age groups.
+    const ageGroups = {
+      '25-': 0,
+      '26-50': 0,
+      '51-75': 0,
+      '76+': 0
+    };
+    const ageCategories = Object.keys(ageGroups);
+    // console.log(ageCategories);
 
-    // const categorizByAge = age => {
-    //   let ageCategory;
-    //   if (age < 26) ageCategory = ageCategories[0];
-    //   else if (age < 51) ageCategory = ageCategories[1];
-    //   else if (age < 76) ageCategory = ageCategories[2];
-    //   else ageCategory = ageCategories[3];
-    //   return ageCategory;
-    // };
-    // const profilesByAgeGroups = profiles.reduce((acc, profile) => {
-    //   const key = categorizByAge(profile.dob.age);
-    //   acc[key] += 1;
-    //   return acc;
-    // }, ageGroups);
-    // console.log(profilesByAgeGroups);
+    const categorizByAge = age => {
+      let ageCategory;
+      if (age < 26) ageCategory = ageCategories[0];
+      else if (age < 51) ageCategory = ageCategories[1];
+      else if (age < 76) ageCategory = ageCategories[2];
+      else ageCategory = ageCategories[3];
+      return ageCategory;
+    };
+    const profilesByAgeGroups = profiles.reduce((acc, profile) => {
+      const key = categorizByAge(profile.dob.age);
+      acc[key] += 1;
+      return acc;
+    }, ageGroups);
+    console.log(profilesByAgeGroups);
   })
   .catch(function(err) {
     console.log(err);
